6fb2df7d6b828cd91c4f990edc35f1eb4e985ba1
Alex Perucchini <alessandrop65@gmail.com>
2012-09-30 08:16:14 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Poetas::Application.config.secret_token = 'd3c5fd916e699070ed5024625c70f9c40f02a9410435148518913b7c5e0911cf628d1ccbe6860b738942fe19c5f7d170d06b8de13e39eec6b4d0b8998529dafb'
