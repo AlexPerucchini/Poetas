360767f57764c63ee42f6963df0c3f397b70066f
Alex Perucchini <alessandrop65@gmail.com>
2012-10-24 09:15:47 -0600

    rspec for sign in user with valid information

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -7,12 +7,12 @@ describe "Authentication" do
   describe "sign in " do
     
     before { visit(signin_path) }
-    it { should have_selector('h1', text: "Sign in") }
+    it { should have_selector('h1',    text: "Sign in") }
     it { should have_selector('title', text: "Poetas - Sign in") }
 
     describe "with invalid information" do
       
-      before { click_button("Sign in")}
+      before { click_button("Sign in") }
 
       it { should have_selector('title', text: 'Sign in') }
       it { should have_selector('div.alert.alert-error', text: 'Invalid') }
@@ -20,6 +20,20 @@ describe "Authentication" do
 
     describe "with valid information" do
       
+      let(:user) { FactoryGirl.create(:user)  }
+
+      before do
+        visit(signin_path)
+        fill_in "Email",    with: "#{user.email}"
+        fill_in "Password", with: "#{user.password}"
+        click_button("Sign in")
+      end
+
+      it { should have_selector('title',  text: "Poetas") }
+      it { should have_selector('h1',     text: "Welcome") }
+      it { should have_selector('small',  text: "Signed in as #{user.name}.") }
+      it { should have_link('Logout') }
+
     end
   end
 
