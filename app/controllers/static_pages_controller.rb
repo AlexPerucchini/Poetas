64d942bf2b0d99ef3da327ceb7a85de850d980e9
Alex Perucchini <alessandrop65@gmail.com>
2012-12-13 10:58:29 -0700

    Updated Poet Profile and worked on Home Page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,9 @@ class StaticPagesController < ApplicationController
   before_filter :authenticate_user!, except: [:home, :about, :contact]
 
   def home
+    @poems_count = Poem.count
+    @poets_count = User.count
+    @latest_poet = User.where("created_at > ?", Time.zone.now - 1.hour).last
   end
 
   def about
--- app/helpers/poems_helper.rb
+++ app/helpers/poems_helper.rb
@@ -1,2 +1,14 @@
 module PoemsHelper
+
+  def text_analyzer(poem)
+
+    poem = poem.body
+    line_count = poem.lines.count 
+    text = poem
+    character_count = text.length
+
+    return "Line count: #{line_count = poem.lines.count }" + "\n"
+           "Character count: #{character_count} /n" + ""
+  
+  end
 end
