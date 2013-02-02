20eddc2e6e5e4a6278acb34a832721fa541f6119
Alex Perucchini <alessandrop65@gmail.com>
2013-02-02 10:59:33 -0700

    Fixed issue with Users model not bein re-index by solr after deactivating user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,12 +1,15 @@
 class Admin::UsersController < AdminController
 
   def index
-       @search = User.search do
-        fulltext params[:search]
+      #we need to force solr to re-index if the a User was deleted or Activated
+    User.reindex
+    @search = User.search do
+      fulltext params[:search]
     end
+    @total_users = User.count
     @users = @search.results
     @deleted_users = User.deleted
-    respond_with(@user)
+
   end
 
   def destroy
@@ -19,6 +22,5 @@ class Admin::UsersController < AdminController
      @user = User.find(params[:id])
      @user.revive
      redirect_to(admin_users_path)
-     #respond_with(@user, :location => admin_users_path)
   end
 end
